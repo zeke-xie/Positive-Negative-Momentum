ed1e563bb12697d5caa050d2be837a1a27de405f
Zeke <584649769@qq.com>
2020-11-07 20:35:43 +0900

    Update adapnm.py

--- adapnm.py
+++ adapnm.py
@@ -5,32 +5,32 @@ from torch.optim.optimizer import Optimizer, required
 
 
 class AdaPNM(Optimizer):
-    r"""Implements Adam with Positive-Negative Momentum.
+    r"""Implements Adaptive Positive-Negative Momentum.
     It has be proposed in 
-    `A Gradient Noise Amplification Method Improves Learning for Deep Networks`__.
+    `Positive-Negative Momentum: A Noise Enhancement Method for Stochastic Optimization`__.
     Arguments:
         params (iterable): iterable of parameters to optimize or dicts defining
             parameter groups
-        lr (float, optional): learning rate (default: 1e-3)
+        lr (float, optional): learning rate (default: 1e-2)
         betas (Tuple[float, float], optional): coefficients used for computing
-            running averages of gradient and its square (default: (0.9,0.999,-1.))
+            running averages of gradient and its square (default: (0.9,0.999, 1.))
         eps (float, optional): term added to the denominator to improve
             numerical stability (default: 1e-8)
-        weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
+        weight_decay (float, optional): decoupled weight decay (default: 0.)
         amsgrad (boolean, optional): whether to use the AMSGrad variant of this
             algorithm from the paper `On the Convergence of Adam and Beyond`_
             (default: False)
     """
 
-    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999, -1.), eps=1e-8,
-                 weight_decay=0, amsgrad=False):
+    def __init__(self, params, lr=1e-2, betas=(0.9, 0.999, 0.5), eps=1e-8,
+                 weight_decay=0., amsgrad=False):
         if not 0.0 <= lr:
             raise ValueError("Invalid learning rate: {}".format(lr))
-        if not 0. <= betas[0] < 1.0:
+        if not 0. < betas[0] < 1.0:
             raise ValueError("Invalid beta parameter at index 0: {}".format(betas[0]))
         if not 0.0 <= betas[1] < 1.0:
             raise ValueError("Invalid beta parameter at index 1: {}".format(betas[1]))
-        if not  betas[2] < 0.:
+        if not betas[2] >= 0.:
             raise ValueError("Invalid beta parameter at index 2: {}".format(betas[2]))
         if not 0.0 <= eps:
             raise ValueError("Invalid epsilon value: {}".format(eps))
@@ -91,7 +91,7 @@ class AdaPNM(Optimizer):
                 beta1, beta2, beta3 = group['betas']
 
                 state['step'] += 1
-                bias_correction1 = 1 - beta1 ** ((state['step'] + 1) / 2)
+                bias_correction1 = 1 - beta1 ** int((state['step'] + 1) / 2)
                 bias_correction2 = 1 - beta2 ** state['step']
 
                 exp_avg_sq = state['exp_avg_sq']
@@ -99,10 +99,10 @@ class AdaPNM(Optimizer):
                     exp_avg, neg_exp_avg = state['exp_avg'], state['neg_exp_avg']
                 else:
                     exp_avg, neg_exp_avg = state['neg_exp_avg'], state['exp_avg']
-
-                # Decay the first and second moment running average coefficient
-                exp_avg.mul_(beta1).add_(grad, alpha=1 - beta1)
-                neg_exp_avg.mul_(beta1)
+                
+                exp_avg.mul_(beta1**2).add_(grad, alpha=1 - beta1**2)
+                noise_norm = math.sqrt((1+beta2) ** 2 + beta2 ** 2)
+                
                 exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
                 if amsgrad:
                     # Maintains the maximum of all 2nd moment running avg. till now
@@ -113,6 +113,7 @@ class AdaPNM(Optimizer):
                     denom = (exp_avg_sq.sqrt() / math.sqrt(bias_correction2)).add_(group['eps'])
 
                 step_size = group['lr'] / bias_correction1
-
-                p.addcdiv_(exp_avg.mul(1-beta3/2.).add(neg_exp_avg,alpha=beta3/2.), denom, value=-step_size)
+                
+                pnmomentum = exp_avg.mul(1+beta3).add(neg_exp_avg,alpha=-beta3).mul(1/noise_norm)
+                p.addcdiv_(pnmomentum, denom, value=-step_size)
         return loss
