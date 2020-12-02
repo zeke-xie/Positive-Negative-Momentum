4d1eea184495baf50162a268fed437c7697191e4
Zeke <584649769@qq.com>
2020-12-02 21:55:58 +0900

    Update adapnm.py

--- pnm_optim/adapnm.py
+++ pnm_optim/adapnm.py
@@ -26,7 +26,7 @@ class AdaPNM(Optimizer):
                  weight_decay=0., amsgrad=True):
         if not 0.0 <= lr:
             raise ValueError("Invalid learning rate: {}".format(lr))
-        if not 0. < betas[0] < 1.0:
+        if not 0. <= betas[0] < 1.0:
             raise ValueError("Invalid beta parameter at index 0: {}".format(betas[0]))
         if not 0.0 <= betas[1] < 1.0:
             raise ValueError("Invalid beta parameter at index 1: {}".format(betas[1]))
